bc445120becd1bc5b0ce5c2cba2144da3fb906f6
unknown <marlon.paulo.silva@outlook.com>
2021-10-21 00:31:47 -0300

    Primeiro commit + criando as Tabs

---
+++ components/HeaderTabs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HeaderTabs() {
+    return (
+        <View>
+            <Text>Header Tabs</Text>
+        </View>
+    );
+}
+
+const HeaderButton = () => (
+    <Text>Delivery</Text>
+)
---
+++ screens/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
+
+import HeaderTabs from '../components/HeaderTabs';
+
+export default function Home() {
+    return (
+        <SafeAreaView style={styles.container} >
+            <HeaderTabs />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      marginTop: 18
+    },
+  });
+  

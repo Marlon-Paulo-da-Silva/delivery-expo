4257496eb418a66e57d5ffe1ca4660e79dc2e7d3
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2021-10-22 07:57:29 -0300

    inserindo ícones no lado direito da barra de busca

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 export default function Searchbar() {
     const [activeTab, setActiveTab] = useState("Delivery");
 
     return (
-        <View style={{flexDirection: "row", marginTop: 15}}>
+        <View style={{flexDirection: "row", marginTop: 15 }}>
           <GooglePlacesAutocomplete
             styles={{
               textInput: {
@@ -21,18 +22,29 @@ export default function Searchbar() {
                 backgroundColor: "#eee",
                 borderRadius: 50,
                 flexDirection: "row",
-                alignContent: "center",
-                marginRight: 10
+                
+                marginRight: 10,
               }
             }} 
             placeholder="Buscar"
             renderLeftButton={() => (
-            <View style={{ marginLeft: 10, alignSelf: "center" }}>
+            <View style={{ marginLeft: 10, alignSelf: "center", }}>
                 <Ionicons name="location-sharp" size={34} />
               </View>
             )}
             renderRightButton={() => (
-              <View></View>
+              <View style={
+                { 
+                  alignSelf: "center",
+                  flexDirection: "row",
+                  marginRight: 8,
+                  backgroundColor: 'white',
+                  padding: 9,
+                  borderRadius: 30,
+                  alignItems: 'center'
+                }}>
+                  <AntDesign name="clockcircle" size={10} style={{marginRight: 6}} />
+                  <Text>Search</Text></View>
             )}
           />
         </View>

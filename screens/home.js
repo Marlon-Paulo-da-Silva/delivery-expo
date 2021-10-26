aba67457064281e9e0a5dc7635395c20ca471d2f
unknown <marlon.paulo.silva@outlook.com>
2021-10-26 01:16:40 -0300

    Adicionando API YELP

--- screens/home.js
+++ screens/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, SafeAreaView, ScrollView } from 'react-native';
 import Categories from '../components/Categories';
 import RestaurantItems, {
@@ -7,9 +7,32 @@ import RestaurantItems, {
 import HeaderTabs from '../components/HeaderTabs';
 import Searchbar from '../components/Searchbar';
 
+const YELP_API_KEY = "lJR3badoEglVFWrT2heKnqSma-OaxKViosds8fU2VguNEUbzohINQpp_tFhKtTd5GUIRFrfAE5BghU1ECYzSWKW2niBtggsyMaGMSr33-zZVmYIwZp2AfsktE3t3YXYx";
+
 export default function Home() {
 
     const [restaurantData, setRestaurantData] = React.useState(localRestaurants);
+    const [city, setCity] = useState("Sao Paulo");
+
+    const getRestaurantsFromYelp = () => {
+        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+
+        const apiOptions = {
+          headers: {
+            Authorization: `Bearer ${YELP_API_KEY}`,
+          },
+        };
+
+        return fetch(yelpUrl, apiOptions)
+        .then((res) => res.json())
+        .then((json) =>
+            setRestaurantData(
+            json.businesses));
+    };
+
+    useEffect(() => {
+        getRestaurantsFromYelp();
+    });
 
     return (
         <SafeAreaView style={styles.container} >
